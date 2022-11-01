cf1d2b784432aeb9545e562f348e37d21920118e
Mtynan <mtynan4@gmail.com>
2022-11-01 00:19:36 +0000

    poly line toggle

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,6 +9,7 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 import { useMemo, useState } from "react";
+import { Button } from "semantic-ui-react";
 import { IMarker } from "../interfaces/IMarker";
 
 type Props = {
@@ -16,57 +17,84 @@ type Props = {
 };
 
 const Map = ({ markers }: Props): JSX.Element => {
+  const [showPolyLine, setShowPolyLine] = useState<boolean>(false);
   const center = useMemo(() => ({ lat: 53.483959, lng: -2.244644 }), []);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GMAPS_API_KEY!,
   });
-  debugger;
+  const [polyOnMap, setPolyOnMap] = useState<google.maps.Polyline[]>([]);
+  const [directionsOnMap, setDirectionsOnMap] = useState<
+    google.maps.DirectionsRenderer[]
+  >([]);
+
+  const addPoly = (p: google.maps.Polyline) => {
+    setPolyOnMap((prev) => [...prev, p]);
+  };
+
+  const addDirection = (d: google.maps.DirectionsRenderer) => {
+    setDirectionsOnMap((prev) => [...prev, d]);
+  };
+
+  const showPoly = () => {
+    if(showPolyLine){
+      removePoly();
+    } else {
+      removeDirections();
+    }
+    setShowPolyLine((prev) => !prev);
+  };
+  const removePoly = () => {
+    polyOnMap.forEach((x) => x.setMap(null));
+    setPolyOnMap([]);
+  };
+  const removeDirections = () => {
+    directionsOnMap.forEach((x) => x.setMap(null));
+    directionsOnMap.forEach((x) => x.setPanel(null));
+    setDirectionsOnMap([]);
+  };
   return (
     <>
       {!isLoaded ? (
         <>Loading map..</>
       ) : (
-        
         <>
-        
-        <GoogleMap
-          data-testId="MaptestId"
-          zoom={10}
-          center={center}
-          mapContainerClassName="map-container"
-        >
-          {markers?.map(({ from, to, id, path, colour, directions }) => (
-            <>
-              <MarkerF key={`${id}_from`} position={from} >
-           
-              </MarkerF>
-              <MarkerF key={`${id}_to`} position={to}></MarkerF>
-              {/* <DistanceMatrixService
-                options={{
-                  destinations: [from],
-                  origins: [to],
-                  travelMode: google.maps.TravelMode.DRIVING,
-                  
-                }}
-                callback={(response) => {
-                  console.log(response);
-                }} */}
+          <Button onClick={showPoly}>
+            {showPolyLine ? "toggle directions" : "toggle polyline"}
+          </Button>
+          <GoogleMap
+            data-testId="MaptestId"
+            zoom={10}
+            center={center}
+            mapContainerClassName="map-container"
+          >
+            {markers?.map(({ from, to, id, path, colour, directions }) => (
+              <>
+                {console.log(path)}
+                <MarkerF key={`${id}_from`} position={from}></MarkerF>
 
-              {/* /> */}
-              <DirectionsRenderer
-                directions={directions ?? undefined}
-                options={{
-                  suppressMarkers: true,
-                  polylineOptions: { 
-                    strokeColor: colour,
-                  },
-                }}
-              />
-            </>
-          ))}
-        </GoogleMap>
+                <MarkerF key={`${id}_to`} position={to}></MarkerF>
+                {showPolyLine ? (
+                  <Polyline
+                    path={path}
+                    options={{ strokeColor: colour }}
+                    onLoad={(e) => addPoly(e)}
+                  />
+                ) : (
+                  <DirectionsRenderer
+                    onLoad={(e) => addDirection(e)}
+                    directions={directions ?? undefined}
+                    options={{
+                      suppressMarkers: true,
+                      polylineOptions: {
+                        strokeColor: colour,
+                      },
+                    }}
+                  />
+                )}
+              </>
+            ))}
+          </GoogleMap>
         </>
-       
       )}
     </>
   );
